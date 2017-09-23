5b626d81ab0ada281431f2316362c5bd4dcd81bb
yhnreddy1 <yhnreddy@Toshiba>
2017-09-23 09:07:49 +0530

    Testing merge conflicts

--- TestProj/src/com/test/loop/TestForLoop.java
+++ TestProj/src/com/test/loop/TestForLoop.java
@@ -4,9 +4,9 @@ public class TestForLoop {
 
 	public static void main(String[] args) {
 		
-		String names[] = {"Siva","Raju","Eswar","Gopi","Rajesh"};
+		String names[] = {"Siva","Raju","Eswar","Gopi","Rajesh","Test","User"};
 		
-		//print the names
+		//print the names with traditional loop
 		for(int i =0; i < names.length; i++){
 			System.out.println("Name :: "+names[i]);
 		}
