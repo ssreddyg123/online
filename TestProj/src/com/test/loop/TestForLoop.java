7200d4681166287a03a40fcc77f919aae80d957c
yhnreddy <yhnreddy@Toshiba>
2017-09-23 08:47:55 +0530

    commiting initial proj

---
+++ TestProj/src/com/test/loop/TestForLoop.java
@@ -0,0 +1,16 @@
+package com.test.loop;
+
+public class TestForLoop {
+
+	public static void main(String[] args) {
+		
+		String names[] = {"Siva","Raju","Eswar","Gopi","Rajesh"};
+		
+		//print the names
+		for(int i =0; i < names.length; i++){
+			System.out.println("Name :: "+names[i]);
+		}
+
+	}
+
+}
